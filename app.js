efaca617c1e5edbcbc1cedaedb56cabbca0e6eb5
Magsz <xmagsz95@gmail.com>
2022-08-15 18:33:41 -0700

    Finished Blog Website, connected to a MongoDB DB

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const req = require("express/lib/request");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+//mongodb+srv://<username>:<password>@cluster0.3ervyg8.mongodb.net/?retryWrites=true&w=majority
+
+mongoose.connect(
+	"mongodb+srv://BlogWebsite:blogwebsitepassword@cluster0.3ervyg8.mongodb.net/?retryWrites=true&w=majority",
+	{ useNewUrlParser: true }
+);
 
 const homeStartingContent =
 	"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,8 +21,6 @@ const aboutContent =
 const contactContent =
 	"Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
 const app = express();
 
 app.set("view engine", "ejs");
@@ -22,8 +28,22 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//* POST SCHEMA
+const postSchema = new mongoose.Schema({
+	title: String,
+	content: String,
+});
+
+//* NEW MODEL
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", (req, res) => {
-	res.render("home", { homeContent: homeStartingContent, newPosts: posts });
+	Post.find({}, (err, posts) => {
+		res.render("home", {
+			homeContent: homeStartingContent,
+			posts: posts,
+		});
+	});
 });
 
 app.get("/about", (req, res) => {
@@ -39,33 +59,27 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-	const newPost = {
+	const post = new Post({
 		title: req.body.postTitle,
 		content: req.body.postBody,
-	};
+	});
 
-	posts.push(newPost);
-
-	res.redirect("/");
-});
-
-app.get("/posts/:postName", (req, res) => {
-	const requestedTitle = _.lowerCase(req.params.postName);
-
-	posts.forEach((post) => {
-		const postTitle = _.lowerCase(post.title);
-
-		if (requestedTitle === postTitle) {
-			res.render("post", {
-				postTitle: post.title,
-				content: post.content,
-			});
+	post.save((err) => {
+		if (!err) {
+			res.redirect("/");
+		} else {
+			console.log("warning");
 		}
 	});
 });
 
-app.get("/post", (req, res) => {
-	res.render("post", { newPosts: posts });
+app.get("/posts/:postId", (req, res) => {
+	const requestedPostId = req.params.postId;
+	console.log(req.params.postId); // testing to make sure id is grabbed from params
+
+	Post.findOne({ _id: requestedPostId }, function (err, post) {
+		res.render("post", { title: post.title, content: post.content });
+	});
 });
 
 app.listen(3000, function () {
